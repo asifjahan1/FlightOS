e949f7b9b7f1364f6ac75e2c4a0ad336b732c8be
ShamsulHaque12 <mdshamsulhaquesujon@gmail.com>
2026-08-04 11:05:55 +0600

    refactor: reorder and organize imports in location service

--- lib/core/location/location_service.dart
+++ lib/core/location/location_service.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math' as math;
-import 'package:http/http.dart' as http;
+
 import 'package:geolocator/geolocator.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:get_it/get_it.dart';
 import 'package:skynav/core/database/daos/airport_dao.dart';
 import 'package:skynav/features/telemetry/domain/entities/telemetry_data.dart';
 
